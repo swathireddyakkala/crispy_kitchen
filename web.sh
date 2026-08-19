2576633865d9250d40a5ad3d80cc1f5ad90b2191
swathireddyakkala <swathiakkala2623@gmail.com>
2026-08-19 12:53:26 +0000

    Second Commit for Crispy_Kitchen Website Add Some Changes

--- web.sh
+++ web.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-
 sudo apt update
-sudo apt install -y
+sudo apt install wget net-tools unzip figlet apache2 -y
+sudo systemctl start apache2
+sudo systemctl enable apache2
+mkdir -p webfiles
+cd webfiles
+sudo wget https://www.tooplate.com/zip-templates/2129_crispy_kitchen.zip
+sudo unzip -o 2129_crispy_kitchen.zip
+sudo rm -rf /var/www/html/*
+sudo cp -r 2129_crispy_kitchen/* /var/www/html/
+cd ..
+sudo rm -rf webfiles
+sudo systemctl restart apache2
+figlet done
